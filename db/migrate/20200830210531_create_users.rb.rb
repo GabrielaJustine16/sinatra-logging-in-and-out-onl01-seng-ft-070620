00913161059858eed0ca0358cb6303d06eb23d64
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-30 21:05:55 +0000

    Automatically backed up by Learn

--- db/migrate/20200830210531_create_users.rb.rb
+++ db/migrate/20200830210531_create_users.rb.rb
@@ -1,4 +1,9 @@
-class CreateUsers.rb < ActiveRecord::Migration[5.1]
+class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :balance
+    end
   end
 end
